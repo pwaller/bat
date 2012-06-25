5e9950b126c963e2ccfd5c6916ce0ddb3ff7f754
Peter Waller <peter.waller@cern.ch>
2012-06-25 20:53:23 +0100

    mkscripts: SuperSyst

--- mkscripts.sh
+++ mkscripts.sh
@@ -3,8 +3,11 @@
 RUN=$1
 OF=scripts/run_${RUN}.sh
 
-echo "cd /user1/pwaller/master-a4/bumps/bat" > $OF
-echo "build/diphoton2012-limitsetting $RUN 1 100 $RUN 1 1 1 1 0 0 1 output IsoTemplate_c0.01_FullSyst &> log_${RUN}" >> $OF
+echo > $OF
+
+echo "#PBS -l walltime=24:00:00" >> $OF
+echo "cd /user1/pwaller/master-a4/bumps/bat" >> $OF
+echo "build/diphoton2012-limitsetting $RUN 1 1000 $RUN 1 1 1 1 0 0 1 output-supersyst IsoTemplate_c0.01_SuperSyst &> log_${RUN}" >> $OF
 
 chmod u+x scripts/run_${RUN}.sh
 
